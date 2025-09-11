c6a1cff95871288ae484efd878a2ec6b8685d201
lokeshgit1 <chlokeshreddy.2.0@gmail.com>
2025-09-11 17:47:53 +0530

    db connted & env.js completed

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,6 +2,13 @@ import "dotenv/config";
 
 export const ENV ={
     PORT: process.env.PORT || 5000,
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/chat-app',
-    MODE_ENV: process.env.MODE_ENV || 'development'
-}
+    MONGODB_URI: process.env.MONGODB_URI,
+    MODE_ENV: process.env.MODE_ENV,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    INNGEST_API_KEY: process.env.INNGEST_API_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+}; 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import { ENV } from './config/env.js';
+import { connetDB } from './config/db.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-
-console.log('Mongo URI:', ENV.MONGODB_URI);
+ 
 
 app.listen(ENV.PORT, () => {
+   connetDB().then(() => {
+        console.log('Database connected');
+    }).catch((error) => {
+        console.error('Database connection failed:', error);
+    });
   console.log(`Server is running on port http://localhost:${ENV.PORT}`);
 });
