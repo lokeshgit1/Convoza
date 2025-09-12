6341460d820337e9923a5794966577fe0b1a5d79
lokeshgit1 <chlokeshreddy.2.0@gmail.com>
2025-09-12 15:27:03 +0530

    auth implemented

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,39 @@
+import { Inngest } from "inngest";
+import { connetDB } from "./db.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "my-app" });
+
+const syncUser = inngest.createFunction(
+    {id: "sync-user"}, 
+    {event: "Sync/user.created"},
+    async ({event}) => {
+        await connetDB();
+        const {id, email_addresses, first_name, last_name, image_url} = event.data;
+        const newUser ={
+            clerkId: id,
+            email: email_addresses[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}`,
+            image: image_url,
+        }
+        await User.create({ newUser }) 
+
+        //todo do more thinngs here like send welcome email etc
+    }
+);
+
+const deleteUserFromDB = inngest.createFunction(
+    {id: "delete-user-from-db"}, 
+    {event: "Sync/user.deleted"},
+    async ({event}) => {
+        await connetDB();
+        const {id} = event.data;
+        await User.deleteOne({clerkId: id});
+       // await deleteStreamUser(id.toString());
+
+        //todo do more thinngs here like send welcome email etc
+    }
+);
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUser,deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,39 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connetDB } from './config/db.js';
+import {clerkMiddleware} from '@clerk/express';
+import { functions, inngest } from './config/inngest.js';
+import {serve} from "inngest/express";
 
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware()); // req.auth will be available in the request object  
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
  
 
-app.listen(ENV.PORT, () => {
-   connetDB().then(() => {
-        console.log('Database connected');
-    }).catch((error) => {
-        console.error('Database connection failed:', error);
-    });
-  console.log(`Server is running on port http://localhost:${ENV.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connetDB();
+    if ( ENV.MODE_ENV != " production")
+    {
+      app.listen(ENV.PORT, () => {
+        console.log("server started on port :", ENV.PORT);
+      })
+    }
+  }catch(error)
+  {
+    console.error("error starting server: ", error)
+    process.exit(1);
+  }
+}
+
+startServer();
+
+export default app;
