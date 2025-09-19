a958ad13809e82323f7871f51e0db8083165f95a
lokeshgit1 <chlokeshreddy.2.0@gmail.com>
2025-09-19 20:09:56 +0530

    fixed bugs

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { upsertStreamUser, deleteStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
@@ -27,7 +28,7 @@ const syncUser = inngest.createFunction(
             image: newUser.image,
         });
 
-        //await addUserToPublicChannels(newUser.clerkId.toString());
+        // await addUserToPublicChannels(newUser.clerkId.toString());
     }
 );
 
--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -2,11 +2,10 @@ import { generateStreamToken } from "../config/stream.js";
 
 export const getStreamToken = async (req, res) => {
     try {
-        const token =   generateStreamToken(req.auth().userId);
-
+        const token = generateStreamToken(req.auth().userId); 
         res.status(200).json({ token });
     } catch (error) {
         console.error("Error generating stream token:", error);
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Failed to generate token" });
     }
 };
