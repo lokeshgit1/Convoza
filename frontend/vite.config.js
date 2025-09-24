ee206abcc24774d142984ee8c08c759a194b4576
lokeshgit1 <chlokeshreddy.2.0@gmail.com>
2025-09-24 18:27:34 +0530

    vite.config.js changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // raise the warning threshold (value is in KB)
+    chunkSizeWarningLimit: 1500
+  }
 })
